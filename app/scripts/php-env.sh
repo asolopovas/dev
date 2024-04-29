237880720306fad0d71dfc951f2370ca15a33a78
andrius <andrius.solopovas@gmail.com>
2024-04-29 14:19:15 +0100

    save

--- app/scripts/php-env.sh
+++ app/scripts/php-env.sh
@@ -2,6 +2,7 @@
 
 [ ! -f /var/log/xdebug.log ] && touch /var/log/xdebug.log && chown www:www /var/log/xdebug.log
 [ ! -d /var/www/xdebug ] && mkdir /var/www/xdebug && chown -R www:www /var/www/xdebug
+[ ! -d /var/www/access.log ] && touch /var/www/access.log && chown -R www:www /var/www/access.log
 
 PHP_FPM_CONF_DIR="/usr/local/etc/php-fpm.d"
 
@@ -13,14 +14,12 @@ fi
 
 sed -i "s/XDEBUG_IDEKEY/${XDEBUG_IDEKEY}/g;s/XDEBUG_HOST/${XDEBUG_HOST}/g;s/XDEBUG_MODE/${XDEBUG_MODE}/g;s/XDEBUG_TRIGGER/${XDEBUG_TRIGGER}/g" $PHP_INI_DIR/conf.d/xdebug.ini
 sed -i "s/user = www-data/user = ${APP_USER}/g;s/group = www-data/group = ${APP_USER}/g;" /usr/local/etc/php-fpm.d/www.conf
-
 sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 64M/g" $PHP_INI_DIR/php.ini
 sed -i "s/post_max_size = 8M/post_max_size = 64M/g" $PHP_INI_DIR/php.ini
 sed -i "s/memory_limit = 128M/memory_limit = 1024M/g" $PHP_INI_DIR/php.ini
+sed -i "s#access.log = \/proc\/self\/fd\/2#access.log = \/var\/www\/access.log#g" $PHP_FPM_CONF_DIR/docker.conf
 sed -i "s/pm.max_children = 5/pm.max_children = 50/g" $PHP_FPM_CONF_DIR/www.conf
 sed -i "s/pm.start_servers = 2/pm.start_servers = 5/g" $PHP_FPM_CONF_DIR/www.conf
 sed -i "s/pm.max_spare_servers = 3/pm.max_spare_servers = 5/g" $PHP_FPM_CONF_DIR/www.conf
 sed -i "s/pm.min_spare_servers = 1/pm.min_spare_servers = 5/g" $PHP_FPM_CONF_DIR/www.conf
 sed -i "s/max_execution_time = 30/max_execution_time = 60/g" $PHP_INI_DIR/php.ini
-
-
